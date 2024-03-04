6dea873b0556281f225e02ba6e118c7881d7ad61
CintiaEdel <162153923+CintiaEdel@users.noreply.github.com>
2024-03-04 16:50:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function lightMode() {
+    document.getElementById("pageHeader").style.color = "black";
+    document.getElementById("pageHeader").style.background = "rgb(200, 200, 200)";
+    document.getElementById("navigationBar").style.color = "black";
+    document.getElementById("navigationBar").style.background:hover = "rgb(200, 200, 200)";
+
+}
