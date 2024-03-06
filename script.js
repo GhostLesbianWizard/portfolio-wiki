6817963a57df46435017cec0b18c287164875593
CintiaEdel <162153923+CintiaEdel@users.noreply.github.com>
2024-03-06 18:41:14 +0100

    added light mode and dark mode toggle

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function lightMode() {
-    document.getElementById("pageHeader").style.color = "black";
-    document.getElementById("pageHeader").style.background = "rgb(200, 200, 200)";
-    document.getElementById("navigationBar").style.color = "black";
-    document.getElementById("navigationBar").style.background:hover = "rgb(200, 200, 200)";
-
+    if (document.getElementById("lightSwitch").innerHTML == "Light Mode") {
+        document.getElementById("lightSwitch").innerHTML = "Dark Mode";
+    } else {
+        document.getElementById("lightSwitch").innerHTML = "Light Mode";
+    }
+    document.body.classList.toggle("lightMode");
 }
